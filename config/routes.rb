ad5bf70772c9aad447bbe4d1f31d72a79ef612f7
Xiao Jia <stfairy@gmail.com>
2011-08-17 20:12:16 +0800

    only post method allowed for creating new revision of a page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ EnterpriseWiki::Application.routes.draw do
   root :to => redirect('/Home')
   
   match '*path/edit' => 'page#edit', :via => :get
-  match '*path'      => 'page#save', :via => [:put, :post]
+  match '*path'      => 'page#save', :via => :post
   match '*path'      => 'page#show', :via => :get
 end
