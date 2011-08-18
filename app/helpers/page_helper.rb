4ca2ce03eaee6be32b283e121db16ed6bb35c9b7
Xiao Jia <stfairy@gmail.com>
2011-08-18 19:32:14 +0800

    refactor page helper

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -10,28 +10,49 @@ module PageHelper
       "<a href=\"/#{ PageHelper.replace_spaces_in path }\">#{text}</a>"
     end
   end
+  
+  MARKDOWN_OPTIONS = {
+    :no_intra_emphasis   => true,
+    :tables              => true,
+    :fenced_code_blocks  => true,
+    :autolink            => false,
+    :strikethrough       => true,
+    :lax_html_blocks     => false,
+    :space_after_headers => false,
+    :superscript         => false
+  }
+  
+  def render_markdown (body)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, MARKDOWN_OPTIONS).render(body)
+  end
+  
+  def render_textile (body)
+    RedCloth.new(body).to_html
+  end
+  
+  def render_rdoc (body)
+    RDoc::Markup::ToHtml.new.convert(body)
+  end
+  
+  def render_orgmode (body)
+    Orgmode::Parser.new(body).to_html
+  end
+  
+  def render_creole (body)
+    Creole.creolize(body)
+  end
+  
+  def render_mediawiki (body)
+    MediaWiki.new(:data => body).to_html(:noedit => true)
+  end
 
   def render_markup (markup, body)
-    if markup.is :markdown
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-          :no_intra_emphasis   => true,
-          :tables              => true,
-          :fenced_code_blocks  => true,
-          :autolink            => false,
-          :strikethrough       => true,
-          :lax_html_blocks     => false,
-          :space_after_headers => false,
-          :superscript         => false).render(body).html_safe
-    elsif markup.is :textile
-      RedCloth.new(body).to_html.html_safe
-    elsif markup.is :rdoc
-      RDoc::Markup::ToHtml.new.convert(body).html_safe
-    elsif markup.is :orgmode
-      Orgmode::Parser.new(body).to_html.html_safe
-    elsif markup.is :creole
-      Creole.creolize(body).html_safe
-    elsif markup.is :mediawiki
-      MediaWiki.new(:data => body).to_html(:noedit => true).html_safe
+    if    markup.is :markdown  then render_markdown(body).html_safe
+    elsif markup.is :textile   then render_textile(body).html_safe
+    elsif markup.is :rdoc      then render_rdoc(body).html_safe
+    elsif markup.is :orgmode   then render_orgmode(body).html_safe
+    elsif markup.is :creole    then render_creole(body).html_safe
+    elsif markup.is :mediawiki then render_mediawiki(body).html_safe
     else
       body
     end
