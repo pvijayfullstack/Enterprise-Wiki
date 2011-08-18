8da63318c2e041c8ad9f71e5c2586a2a432737f6
Xiao Jia <stfairy@gmail.com>
2011-08-18 19:23:11 +0800

    add support for plain text

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -21,19 +21,19 @@ module PageHelper
           :strikethrough       => true,
           :lax_html_blocks     => false,
           :space_after_headers => false,
-          :superscript         => false).render(body)
+          :superscript         => false).render(body).html_safe
     elsif markup.is :textile
-      RedCloth.new(body).to_html
+      RedCloth.new(body).to_html.html_safe
     elsif markup.is :rdoc
-      RDoc::Markup::ToHtml.new.convert(body)
+      RDoc::Markup::ToHtml.new.convert(body).html_safe
     elsif markup.is :orgmode
-      Orgmode::Parser.new(body).to_html
+      Orgmode::Parser.new(body).to_html.html_safe
     elsif markup.is :creole
-      Creole.creolize(body)
+      Creole.creolize(body).html_safe
     elsif markup.is :mediawiki
-      MediaWiki.new(:data => body).to_html(:noedit => true)
+      MediaWiki.new(:data => body).to_html(:noedit => true).html_safe
     else
-      # TODO escape as plain text
+      body
     end
   end
 end
