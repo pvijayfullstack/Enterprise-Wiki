e314c9301e77697a2cd38b339e13b70f7d53bdc8
Xiao Jia <stfairy@gmail.com>
2011-08-31 09:30:56 +0800

    fix css style of topic_nav heading and clean up routes config

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ EnterpriseWiki::Application.routes.draw do
   
   root :to => redirect('/home')
   
-  # match '/test/download' => 'test#download'
-  # match '/test/upload'   => 'test#upload'
-  
   # do not start with "admin", 
   # or do not start with "admin/" 
   # (which may be "admin123")
-  page_constraints = { :path => /(?!(admin|themes|users|test)).*|(admin|themes|users|test)(?!\/).+/ }
+  page_constraints = { :path => /(?!(admin|themes|users)).*|(admin|themes|users)(?!\/).+/ }
   
   match '*path' => 'page#view', :constraints => page_constraints, :via => :get
   match '*path' => 'page#save', :constraints => page_constraints, :via => [:put, :post]
