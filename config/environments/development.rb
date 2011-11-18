58e284f862b61ae6b701988b7e602e5f973d72d5
Xiao Jia <stfairy@gmail.com>
2011-11-18 11:59:14 +0800

    remove x-sendfile in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ EnterpriseWiki::Application.configure do
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
   
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
